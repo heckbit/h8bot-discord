65de75939b6a0c4587867448b914105bdf9b2558
Jacob Goudy <43557372+jacobgoudy@users.noreply.github.com>
2021-02-10 02:26:42 -0500

    Updated intents

--- discord-core.js
+++ discord-core.js
@@ -1,7 +1,26 @@
 // Creating the client
 const Discord = require("discord.js");
+// const client = new Discord.Client();
 const client = new Discord.Client({
-  ws: { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_PRESENCES"] },
+  ws: {
+    intents: [
+      `GUILDS`,
+      `GUILD_MEMBERS`,
+      `GUILD_BANS`,
+      `GUILD_EMOJIS`,
+      `GUILD_INTEGRATIONS`,
+      `GUILD_WEBHOOKS`,
+      `GUILD_INVITES`,
+      `GUILD_VOICE_STATES`,
+      `GUILD_PRESENCES`,
+      `GUILD_MESSAGES`,
+      `GUILD_MESSAGE_REACTIONS`,
+      `GUILD_MESSAGE_TYPING`,
+      `DIRECT_MESSAGES`,
+      `DIRECT_MESSAGE_REACTIONS`,
+      `DIRECT_MESSAGE_TYPING`,
+    ],
+  },
 });
 const fs = require("fs");
 const token = fs.readFileSync("./token.txt").toString();
@@ -10,6 +29,7 @@ client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
 ["command-handler", "event-handler"].forEach((handler) => {
+  console.log(`>>> handler loaded`);
   require(`./handlers/${handler}`)(client, Discord);
 });
 
